f401a911c8468b9aaaa4ecd9b09f5e3d338c666d
DXDRILLBASSX <askerka110038302@gmail.com>
2024-04-11 07:29:55 +0300

    0.1

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import disnake
 from disnake.ext import commands
+from typing import Optional
 
-bot = commands.Bot(command_prefix='!', help_command=None, intents=disnake.Intents.all(), test_guilds=[1227760104963837952])
+bot = commands.Bot(command_prefix=commands.when_mentioned, help_command=None, intents=disnake.Intents.all(), test_guilds=[1227760104963837952])
 
-CENSORED_WORDS = ['блять', 'сука']
+CENSORED_WORDS = ['блять', 'сука'] #Список цензуры потом отдельным файликом сделаю
 
+#Логирование в консоль что бот запустился
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user} is ready to work.")
 
+#Уведа о новичке
 @bot.event
 async def on_member_join(member):
     role = disnake.utils.get(member.guild.roles, id=1227772107312595055)
@@ -23,6 +26,7 @@ async def on_member_join(member):
     await member.add_roles(role)
     await channel.send(embed=embed)
 
+#Фильтр сквернословия
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
@@ -33,6 +37,7 @@ async def on_message(message):
                 await message.delete()
                 await message.channel.send(f"{message.author.mention} такие слова запрещены!")
 
+#Проверочка на права и на синтаксисы
 @bot.event
 async def on_command_error(ctx, error):
     print(error)
@@ -44,6 +49,7 @@ async def on_command_error(ctx, error):
             description=f"Правильное использование команды: `{ctx.prefix}{ctx.command.name}`\nExample: {ctx.prefix}{ctx.command.usage}"
         ))
 
+#Кик
 @bot.command(name="кик", aliases=["кыш", "выгнать"], usage="kick <@user> <reason=None>")
 @commands.has_permissions(kick_members=True, administrator=True)
 async def kick(ctx, member: disnake.Member, *, reason="Нарушение правил!"):
@@ -51,6 +57,7 @@ async def kick(ctx, member: disnake.Member, *, reason="Нарушение пра
     await member.kick(reason=reason)
     await ctx.message.delete()
 
+#Бан
 @bot.command(name="бан", aliases=["баня", "банан"], usage="ban <@user> <reason=None>")
 @commands.has_permissions(ban_members=True, administrator=True)
 async def ban(ctx, member: disnake.Member, *, reason="Нарушение правил!"):
@@ -58,6 +65,7 @@ async def ban(ctx, member: disnake.Member, *, reason="Нарушение пра�
     await member.ban(reason=reason)
     await ctx.message.delete()
 
+#Калькулятор
 @bot.slash_command(description="Обычный, простой калькулятор:)")
 async def calc(inter, a: int, oper: str, b: int):
     if oper == "+":
@@ -73,4 +81,42 @@ async def calc(inter, a: int, oper: str, b: int):
 
     await inter.send(str(result))
 
+#Кнопочки
+class Confirm(disnake.ui.View):
+    def __init__(self):
+        super().__init__(timeout=10.0)
+        self.value = Optional[bool]
+
+    @disnake.ui.button(label="Принять", style=disnake.ButtonStyle.green, emoji="😎")
+    async def confirm(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
+        await inter.response.send_message("Отлично, жди ссылку на вступление!")
+        self.value = True
+        self.stop()
+
+    @disnake.ui.button(label="Отмена", style=disnake.ButtonStyle.red, emoji="🥱")
+    async def cancel(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
+        await inter.response.send_message("Хорошо, я тебя поняла")
+        self.value = False
+        self.stop()
+
+class LinkToParty(disnake.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(disnake.ui.Button(label="Жмякни", url="https://youtu.be/dQw4w9WgXcQ?si=xO7MQZLCj0_cy8W5", style=disnake.ButtonStyle.green, emoji="😘"))
+
+@bot.command(name="party")
+async def ask_party(ctx):
+    view = Confirm()
+
+    await ctx.send("Приглашение на вечеринку, согласен ли ты принять его?", view=view)
+    await view.wait()
+
+    if view.value is None:
+        await ctx.send("Ты упустил свой шанс!")
+    elif view.value:
+        await ctx.send("Отлично, держи ссылку!", view=LinkToParty)
+    else:
+        await ctx.send("Ну и хуй тебе")
+
+#Токен
 bot.run('MTIyNjkzNzYwMjY2NjQwMTgxMg.G0VaZu.nik9AnmESFU5gif3hXR2Mmk4LFH3sDTeFFG_IM')
